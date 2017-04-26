4568fbd5859f48d85c7c833f69f8ea7bc1fe0a5d
liu9293 <LiuKai@moodys.com>
2017-04-26 12:42:15 +0800

    move css file in html directly

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import store from 'Redux/store';
 import { Provider } from 'react-redux';
 import AppRoute from './route';
-import './app.css';
 
 class App extends Component {
   render() {
